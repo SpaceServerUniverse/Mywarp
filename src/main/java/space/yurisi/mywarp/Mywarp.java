928556dc0a2c4a3eeefe3f5b9de37fd8c4d4c1bc
yurisi <Dev.yurisi@gmail.com>
2023-10-05 08:13:48 +0900

    add config

--- src/main/java/space/yurisi/mywarp/Mywarp.java
+++ src/main/java/space/yurisi/mywarp/Mywarp.java
@@ -3,6 +3,7 @@ package space.yurisi.mywarp;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import space.yurisi.mywarp.connector.UniverseCoreAPIConnector;
+import space.yurisi.mywarp.file.Config;
 import space.yurisi.universecore.UniverseCoreAPI;
 import space.yurisi.universecore.database.DatabaseManager;
 import space.yurisi.mywarp.command.MywarpCommandManagaer;
@@ -12,16 +13,23 @@ public final class Mywarp extends JavaPlugin {
 
     private UniverseCoreAPIConnector connector;
 
+    private Config config;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
         DatabaseManager manager = UniverseCoreAPI.getInstance().getDatabaseManager();
         this.connector = new UniverseCoreAPIConnector(manager);
+        this.config = new Config(this);
         new MywarpCommandManagaer(this);
     }
 
     public UniverseCoreAPIConnector getConnector(){
         return this.connector;
+    }
+
+    public Config getPluginConfig(){
+        return this.config;
     }
 
     @Override
